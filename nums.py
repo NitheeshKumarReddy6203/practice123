e85ebcd64b35c386f2588e88c7ebb61f90638d10
EC2 Default User <ec2-user@ip-172-31-83-235.ec2.internal>
2024-11-19 05:37:14 +0000

    9Nums.py if modified from LOcal

--- nums.py
+++ nums.py
@@ -1,7 +1,7 @@
 print("\nThis is from the feature-dev Branch\n\n")
 
 for i in range(0,5):
-    print("Iteration Number is :", (i+1)
+    print("Iteration Number is :", (i+1))
 
 print("Here is the end of the feature-Dev Branch and the Next steps are to be Continued")
 
